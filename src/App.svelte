141139ef4ad5c781c3afffd9b15939c1c879338a
Brian Morearty <brian@morearty.org>
2020-06-22 19:43:51 -0700

    initial

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+
+	})
+</script>
+
+<main>
+	<h1>Edit this div:</h1>
+	<div>
+		<div id="editable"></div>
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
